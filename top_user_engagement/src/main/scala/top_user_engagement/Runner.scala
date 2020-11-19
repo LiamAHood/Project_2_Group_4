70dd62bac7d2213a6c38eb74211b4612f86b366d
LiamAHood <liam.hood@revature.net>
2020-11-19 12:46:43 -0800

    updated top user engagement with followers

--- top_user_engagement/src/main/scala/top_user_engagement/Runner.scala
+++ top_user_engagement/src/main/scala/top_user_engagement/Runner.scala
@@ -28,44 +28,87 @@ object Runner {
 //    val userSchema = StructType(List(StructField("UserName", StringType, true)))
 //    var df = spark.createDataFrame(userRDD, userSchema)
 //    df.show()
+    """https://en.wikipedia.org/wiki/List_of_most-followed_Twitter_accounts"""
     val topUsers = List("BarackObama", "justinbieber", "katyperry", "rihanna", "Cristiano",
-          "realDonaldTrump", "taylorswift13", "ladygaga", "TheEllenShow", "ArianaGrande")
+          "realDonaldTrump", "taylorswift13", "ladygaga", "TheEllenShow", "ArianaGrande",
+          "YouTube", "KimKardashian", "jtimberlake", "selenagomez", "narendramodi",
+          "cnnbrk", "twitter", "britneyspears", "ddlovato", "shakira")
     val tweetFields = "author_id,public_metrics,referenced_tweets,source,text,created_at"
     val fields = s"tweet.fields=$tweetFields"
     val dirname = "rawTweets"
     //recent_search.tweetsByUsers(bearerToken = bearerToken, userNames = topUsers, fields = fields, destName = dirname, tweetsPerFile = 100)
 
 
-    val allTweetMetrics = spark.read.option("header", "true").json(s"${dirname}")
-      .select($"username", explode($"data"))
-      .select($"username", $"col.public_metrics.like_count" as "likes", $"col.public_metrics.quote_count" as "quotes",
-        $"col.public_metrics.retweet_count" as "retweets", $"col.public_metrics.reply_count" as "replies",
-        $"col.created_at")
+    val userMetrics = spark.read.option("header", "true").json(s"${dirname}_userdata")
+      .select(explode($"data"))
+      .select($"col.name", $"col.username", $"col.public_metrics.followers_count"/1000000 as "Followers_Millions")
+
+    userMetrics.show()
+    userMetrics.printSchema()
+
+    val allTweetMetrics = userMetrics
+      .join(spark.read.option("header", "true").json(s"${dirname}"), "username")
+      .select($"Name", $"username", round($"Followers_Millions", 2) as "Followers_Millions", explode($"data"))
+      .select($"Name", $"username", $"username", $"Followers_Millions",
+        $"col.public_metrics.like_count" as "Likes",
+        $"col.public_metrics.quote_count" as "Quotes",
+        $"col.public_metrics.retweet_count" as "Retweets",
+        $"col.public_metrics.reply_count" as "Replies")
 
     allTweetMetrics.show()
     allTweetMetrics.printSchema()
 
-    val totalTweetMetrics = allTweetMetrics
-      .groupBy($"username")
-      .agg(sum($"likes"), sum($"quotes"), sum($"retweets"), sum($"replies"),
-        sum($"likes" + $"quotes" + $"retweets" + $"replies") as "total_engagement")
-      .sort($"total_engagement" desc)
+    val aggMetrics = allTweetMetrics
+      .groupBy($"Name", $"Followers_Millions")
+      .agg(round(sum($"Likes" + $"Quotes" + $"Retweets" + $"Replies")/1000,1) as "Total_Engagement_Thousands",
+        round(avg($"Likes" + $"Quotes" + $"Retweets" + $"Replies")/1000,1) as "Average_Engagement_Thousands",
+        round(sum($"Likes")/sum($"Likes" + $"Quotes" + $"Retweets" + $"Replies")*100, 1) as "Like_Percent",
+        round(sum($"Quotes")/sum($"Likes" + $"Quotes" + $"Retweets" + $"Replies")*100, 1) as "Quote_Percent",
+        round(sum($"Retweets")/sum($"Likes" + $"Quotes" + $"Retweets" + $"Replies")*100, 1) as "Retweet_Percent",
+        round(sum($"Replies")/sum($"Likes" + $"Quotes" + $"Retweets" + $"Replies")*100, 1) as "Reply_Percent")
+      .withColumn("Total_Engagement_Per_Thous_Followers", round($"Total_Engagement_Thousands"/$"Followers_Millions", 2))
+      .withColumn("Average_Engagement_Per_Thous_Followers", round($"Average_Engagement_Thousands"/$"Followers_Millions", 2))
 
-    val avgTweetMetrics = allTweetMetrics
-      .groupBy($"username")
-      .agg(avg($"likes"), avg($"quotes"), avg($"retweets"), avg($"replies"),
-        avg($"likes" + $"quotes" + $"retweets" + $"replies") as "average_engagement")
-      .sort($"average_engagement" desc)
+    val avgMetrics = aggMetrics.select($"Name", $"Average_Engagement_Thousands", $"Average_Engagement_Per_Thous_Followers")
+      .sort($"Average_Engagement_Thousands" desc)
+    avgMetrics.show()
 
-    totalTweetMetrics.printSchema()
-    totalTweetMetrics.show()
-    avgTweetMetrics.printSchema()
-    avgTweetMetrics.show()
+    val avgMetricsN = avgMetrics.sort($"Average_Engagement_Per_Thous_Followers" desc)
+    avgMetricsN.show()
 
+    val totMetrics = aggMetrics.select($"Name", $"Total_Engagement_Thousands", $"Total_Engagement_Per_Thous_Followers")
+      .sort($"Total_Engagement_Thousands" desc)
+    totMetrics.show()
+
+    val totMetricsN = totMetrics.sort($"Total_Engagement_Per_Thous_Followers" desc)
+    totMetricsN.show()
+
+    val engMetrics = aggMetrics.select($"Name", $"Followers_Millions",
+      $"Like_Percent", $"Quote_Percent", $"Retweet_Percent", $"Reply_Percent")
+      .sort($"Followers_Millions" desc)
+    engMetrics.show()
+
+//    val totalTweetMetrics = allTweetMetrics
+//      .groupBy($"Name", $"Followers_Millions")
+//      .agg(round(sum($"Likes" + $"Quotes" + $"Retweets" + $"Replies")/1000,1) as "Total_Engagement_Thousands",
+//        round(sum($"Likes")/1000/$"Total_Engagement_Thousands"*100, 1) as "Likes_Thousands", round(sum($"Quotes")/1000, 1)as "Quotes_Thousands",
+//        round(sum($"Retweets")/1000, 1) as "Retweets_Thousands", round(sum($"Replies")/1000, 1) as "Replies_Thousands")
+//      .sort($"Total_Engagement_Thousands" desc)
+//
+//    val avgTweetMetrics = allTweetMetrics
+//      .groupBy($"Name", $"Followers_Millions")
+//      .agg(round(avg($"Likes" + $"Quotes" + $"Retweets" + $"Replies")/1000, 1) as "Average_Engagement_Thousands",
+//        round(avg($"Likes")/1000,1), round(avg($"Quotes")/1000,1),
+//        round(avg($"Retweets")/1000,1), round(avg($"Replies")/1000,1))
+//      .sort($"Average_Engagement_Thousands" desc)
+//
+////    totalTweetMetrics.printSchema()
+//    totalTweetMetrics.show()
+////    avgTweetMetrics.printSchema()
+//    avgTweetMetrics.show()
 
     println("the end")
   }
-
 
 
   def parquetWrite(spark: SparkSession, dirname: String) = {
--- top_user_engagement/src/main/scala/top_user_engagement/recent_search.scala
+++ top_user_engagement/src/main/scala/top_user_engagement/recent_search.scala
@@ -12,7 +12,8 @@ import org.apache.http.util.EntityUtils
 import scala.collection.mutable.ListBuffer
 
 object recent_search {
-  def tweetSearch(bearerToken: String, query: String, fields: String, destName: String, tweetsPerFile: Int=100): String = {
+
+  def tweetSearch(bearerToken: String, query: String, fields: String, tweetsPerFile: Int=100): String = {
     /**
      * query is a string following the twitter API rules for queries, and is URL encoded
      * fields need to be a string of all additional information to return ie. "tweet.fields=author_id,..."
@@ -35,19 +36,41 @@ object recent_search {
     }
   }
 
+  def tweetSearchFull(bearerToken: String, query: String, fields: String, destName: String): Unit = {}
+
+  def userSearch(bearerToken: String, userName: String): String = {
+    val httpClient = HttpClients.custom.setDefaultRequestConfig(RequestConfig.custom.setCookieSpec(CookieSpecs.STANDARD).build).build
+
+    val uriBuilder = new URIBuilder(s"https://api.twitter.com/2/users/by?usernames=${userName}&user.fields=public_metrics")
+    val httpGet = new HttpGet(uriBuilder.build)
+    httpGet.setHeader("Authorization", String.format("Bearer %s", bearerToken))
+
+    val response = httpClient.execute(httpGet)
+    val entity = response.getEntity
+
+    if (null != entity) {
+      EntityUtils.toString(entity, "UTF-8")
+    } else {
+      """null"""
+    }
+  }
+
   def tweetsByUsers(bearerToken: String, userNames: List[String], fields: String, destName: String, tweetsPerFile: Int=100): Unit = {
     val namedResponse: ListBuffer[String] = ListBuffer()
+    val userMetrics: ListBuffer[String] = ListBuffer()
     val fileWriter = new PrintWriter(Paths.get("recenttweet.tmp").toFile)
+    val fileWriterUser = new PrintWriter(Paths.get("userdata.tmp").toFile)
 
     for (user <- userNames) {
       val query = s"from:${user}"
-      val searchResponse = tweetSearch(bearerToken, query, fields, user, tweetsPerFile)
+      val searchResponse = tweetSearch(bearerToken, query, fields, tweetsPerFile)
+      userMetrics += userSearch(bearerToken, user)
       namedResponse += s"""{"username":"$user",""" + searchResponse.stripPrefix("{")
     }
 
     for (ii <- 0 to namedResponse.length-1) {
-      println(namedResponse(ii))
       fileWriter.println(namedResponse(ii))
+      fileWriterUser.println(userMetrics(ii))
     }
 
     fileWriter.close()
@@ -55,5 +78,12 @@ object recent_search {
       Paths.get("recenttweet.tmp"),
       Paths.get(s"${destName}")
     )
+    fileWriterUser.close()
+    Files.move(
+      Paths.get("userdata.tmp"),
+      Paths.get(s"${destName}_userdata")
+    )
   }
+
+  def tweetsByHashtags(bearerToken: String, hashtags: List[String], fields: String, destName: String, tweetsPerFile: Int=100): Unit = {}
 }
